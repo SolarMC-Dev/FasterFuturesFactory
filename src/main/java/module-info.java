79df2f86394fff9322a0dedec712a7e78e4c04b8
A248 <theanandbeh@gmail.com>
2021-06-13 10:28:09 -0400

    Declare managed-waits dependency transitively

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module gg.solarmc.futuresfactory {
 	exports gg.solarmc.futuresfactory.queue;
 
 	requires transitive space.arim.omnibus;
-	requires space.arim.managedwaits;
+	requires transitive space.arim.managedwaits;
 	requires com.lmax.disruptor;
 }
